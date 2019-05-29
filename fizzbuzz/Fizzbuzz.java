6abf60af4b5bfdf2bcb2f146086d909d39dae08c
Arima Kaoru <46063788+arimakaoru@users.noreply.github.com>
2019-05-29 10:02:27 +0900

    Add fizzbuzz

---
+++ fizzbuzz/Fizzbuzz.java
@@ -0,0 +1,17 @@
+public class Fizzbuzz {
+    public static void main(String[] atgs) {
+        final int limit = 20;
+
+        for(int i = 0; i <= limit; i++){
+            if(i % 15 == 0){
+                System.out.println("FizzBuzz");
+            }else if(i % 3 == 0){
+                System.out.println("Fizz");
+            }else if(i % 5 == 0){
+                System.out.println("Buzz");
+            }else{
+                System.out.println(i);
+            }
+        }
+    }
+}
